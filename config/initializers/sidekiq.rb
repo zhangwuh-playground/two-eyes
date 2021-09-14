544b603fb3c9a6fdc376b0ce8256b8450ba2c5e3
wuhui.zhang <ziegler@nd.com.cn>
2021-09-14 15:37:33 +0800

    set redis server host

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,3 +2,7 @@ redisHost = ENV["REDIS_HOST"].nil? ? "redis://localhost:6379" : ENV["REDIS_HOST"
 Sidekiq.configure_client do |config|
   config.redis = { url: redisHost }
 end
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: redisHost }
+end
